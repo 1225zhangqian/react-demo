d0ff882a84dbad9e9639484dc78e51db8fbb7cef
QianZhang <1139384196@qq.com>
2020-11-26 17:17:05 +0800

    添加关于scss实现主题切换的方案

--- my-app/src/containers/RouterDemo/RouterDemo.js
+++ my-app/src/containers/RouterDemo/RouterDemo.js
@@ -6,12 +6,6 @@ class RouterDemo extends Component {
     return <div>
       <p>路由用例</p>
       <a href="https://reacttraining.com/react-router/web/example/basic">https://reacttraining.com/react-router/web/example/basic</a>
-      <h1>test</h1>
-      <h1>test-2</h1>
-      <h1>test-last</h1>
-      <ul>
-
-      </ul>
     </div>
   }
 }
