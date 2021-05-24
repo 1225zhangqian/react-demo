5b89ea401b6530439ece317eec98c5e278d4c5c7
QianZhang <qian.zhang@hexagon.com>
2021-05-24 16:36:14 +0800

    add eslint plugin

--- my-app/.eslintrc.js
+++ my-app/.eslintrc.js
@@ -1,57 +1,63 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "amd": true,
-        commonjs: true,
-        node: true
+  // To specify environments in a configuration file
+  env: {
+    browser: true,
+    es2021: true,
+    amd: true,
+    commonjs: true,
+    node: true
+  },
+  // Use preset to get reasonable defaults
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  // Specify the JavaScript language options you want to support
+  parser: 'babel-eslint',
+  // Setting parser options helps ESLint determine what is a parsing error.
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  // Specify the plugin
+  plugins: ['babel', 'react', 'react-hooks'],
+  // Specify settings that will be shared across all the plugin rules
+  settings: {
+    react: {
+      createClass: 'createReactClass', // Regex for Component Factory to use,
+      // default to "createReactClass"
+      pragma: 'React', // Pragma to use, default to "React"
+      fragment: 'Fragment', // Fragment to use (may be a property of <pragma>), default to "Fragment"
+      version: 'detect', // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+      // default to latest and warns if missing
+      // It will default to "detect" in the future
+      flowVersion: '0.53' // Flow version
+    },
+    propWrapperFunctions: [
+      // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+      'forbidExtraProps',
+      { property: 'freeze', object: 'Object' },
+      { property: 'myFavoriteWrapper' }
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    parser: "babel-eslint",
-    "plugins": [
-        "babel",
-        "react"
-    ],
-    "settings": {
-        "react": {
-            "createClass": "createReactClass", // Regex for Component Factory to use,
-            // default to "createReactClass"
-            "pragma": "React",  // Pragma to use, default to "React"
-            "fragment": "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
-            "version": "detect", // React version. "detect" automatically picks the version you have installed.
-            // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
-            // default to latest and warns if missing
-            // It will default to "detect" in the future
-            "flowVersion": "0.53" // Flow version
-        },
-        "propWrapperFunctions": [
-            // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
-            "forbidExtraProps",
-            { "property": "freeze", "object": "Object" },
-            { "property": "myFavoriteWrapper" }
-        ],
-        "linkComponents": [
-            // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
-            "Hyperlink",
-            { "name": "Link", "linkAttribute": "to" }
-        ]
-    },
-    "rules": {
-        "react/jsx-uses-react": "error",
-        "react/jsx-uses-vars": "error",
-        "react/prop-types": "off",
-        "react/display-name": "off",
-        "no-unused-vars": "warn",
-        "no-console": "off",
-    }
+    linkComponents: [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      'Hyperlink',
+      { name: 'Link', linkAttribute: 'to' }
+    ]
+  },
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 };
