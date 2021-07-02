777779484f2603b06a406b00b29cbab26bb75160
QianZhang <1139384196@qq.com>
2021-07-02 15:04:02 +0800

    update the function of ThemeProvider example

--- my-app/src/containers/ThemeProvider/theme/demo.js
+++ my-app/src/containers/ThemeProvider/theme/demo.js
@@ -1,25 +1,38 @@
-import React, { Component } from "react"
+import React, { useState } from 'react';
 import { Switch } from 'antd';
-import "./demo.scss";
-class ThemeDemo extends Component {
-    state = {
-        theme: "dark"
+import './demo.scss';
+import variables from '../style/_variables.scss';
+let ThemeList = variables;
+Object.keys(variables).forEach(i => {
+  const arr = variables[i].split(' 0 ');
+  ThemeList[i] = arr.map((j, index) => {
+    if (j && index === arr.length - 1) {
+      j = j.substring(0, j.length - 1);
     }
-    onChange = () => {
-        this.setState({ theme: this.state.theme === "dark" ? "light" : "dark" })
-    }
-    render() {
-        return <div >
-            <Switch defaultChecked onChange={this.onChange} />  {this.state.theme}
-            <p>example:</p>
-            <div className="react-ui-theme" data-theme={this.state.theme}>
-                <div className="box-wrap">
-                    <div className="box-content"></div>
-                </div>
-            </div>
+    return j.replace(/ 1 /, ': ');
+  });
+});
+const ThemeDemo = () => {
+  const [theme, setTheme] = useState('dark');
+  const onChange = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+  return (
+    <div>
+      <Switch defaultChecked onChange={onChange} /> {theme}
+      <p>example:</p>
+      <div className="react-ui-theme" data-theme={theme}>
+        <div className="box-wrap">
+          <div className="box-content"></div>
         </div>
-    }
-}
+        <div>
+          {ThemeList[theme]?.map(i => (
+            <p>{i}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-export default ThemeDemo
+export default ThemeDemo;
